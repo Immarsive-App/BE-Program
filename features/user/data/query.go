434bc4b81367c070e2c71b47c4acb3e9f89e0c9a
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-09-04 22:45:20 +0700

    add login

--- features/user/data/query.go
+++ features/user/data/query.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"errors"
 	"kelompok1/immersive-dash/features/user"
 
+	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
 
@@ -14,4 +16,17 @@ func New(db *gorm.DB) user.UserDataInterface {
 	return &userQuery{
 		db: db,
 	}
+}
+
+// Login implements user.UserDataInterface.
+func (r *userQuery) Login(email string) (user.CoreUser, error) {
+	var dataUser User
+	tx := r.db.Where("email=?", email).First(&dataUser)
+	if tx.Error != nil {
+		log.Error("Database error:", tx.Error)
+		return user.CoreUser{}, errors.New(tx.Error.Error() + ", invalid email")
+	}
+
+	dataCore := ModelToCore(dataUser)
+	return dataCore, nil
 }
--- features/user/service/logic.go
+++ features/user/service/logic.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"errors"
+	"kelompok1/immersive-dash/app/middlewares"
 	"kelompok1/immersive-dash/features/user"
+	"kelompok1/immersive-dash/helpers"
+
+	"github.com/labstack/gommon/log"
 )
 
 type userService struct {
@@ -12,4 +17,29 @@ func New(repo user.UserDataInterface) user.UserServiceInterface {
 	return &userService{
 		userData: repo,
 	}
+}
+
+// Login implements user.UserServiceInterface.
+func (s *userService) Login(email string, password string) (user.CoreUser, string, error) {
+	if email == "" {
+		return user.CoreUser{}, "", errors.New("email is required")
+	}
+	if password == "" {
+		return user.CoreUser{}, "", errors.New("password is required")
+	}
+	dataLogin, err := s.userData.Login(email)
+	if err != nil {
+		log.Error("Service error:", err)
+		return user.CoreUser{}, "", err
+	}
+	checkPassword := helpers.ComparePassword(password, dataLogin.Password)
+	if !checkPassword {
+		return user.CoreUser{}, "", errors.New("login failed, wrong password")
+	}
+	token, err := middlewares.CreateToken(int(dataLogin.ID))
+	if err != nil {
+		log.Error("Create token error:", err)
+		return user.CoreUser{}, "", err
+	}
+	return dataLogin, token, nil
 }
